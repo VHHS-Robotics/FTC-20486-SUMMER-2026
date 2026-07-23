fb378d6aeed0e06ac50f31fb68624a06b7f77617
techandrobots <vhhsrobotics@gmail.com>
2026-07-23 13:04:17 -0700

    practis code 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practis.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@TeleOp(name = "Practis", group = "Practis")
 public class Practis extends OpMode {
     public int move = 13;
     DcMotor leftfront;
