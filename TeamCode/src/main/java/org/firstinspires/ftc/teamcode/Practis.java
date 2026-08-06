578e2ec35a285d6d300dbba58469a9685f14f5e0
techandrobots <vhhsrobotics@gmail.com>
2026-08-06 10:40:14 -0700

    practis code 27 + C&P 8 + prank 8

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practis.java
@@ -41,6 +41,36 @@ public class Practis extends OpMode {
             LF.setDirection(DcMotorSimple.Direction.FORWARD);
             LR.setDirection(DcMotorSimple.Direction.FORWARD);
             RF.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        if (gamepad1.bWasPressed()) {
+            RR.setDirection(DcMotorSimple.Direction.REVERSE);
+            LF.setDirection(DcMotorSimple.Direction.FORWARD);
+            LR.setDirection(DcMotorSimple.Direction.REVERSE);
+            RF.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        if (gamepad1.bWasReleased()) {
+            RR.setDirection(DcMotorSimple.Direction.FORWARD);
+            LF.setDirection(DcMotorSimple.Direction.FORWARD);
+            LR.setDirection(DcMotorSimple.Direction.FORWARD);
+            RF.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        if (gamepad1.xWasPressed()) {
+            RR.setPower(1);
+            LF.setDirection(DcMotorSimple.Direction.REVERSE);
+            LR.setPower(-1);
+            RF.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+            if (gamepad1.aWasReleased()) {
+                RR.setDirection(DcMotorSimple.Direction.REVERSE);
+                LF.setDirection(DcMotorSimple.Direction.REVERSE);
+                LR.setDirection(DcMotorSimple.Direction.REVERSE);
+                RF.setDirection(DcMotorSimple.Direction.FORWARD);
+                RR.setPower(1);
+                LF.setPower(1);
+                LR.setPower(1);
+                RF.setPower(1);
+
         }
 /*
         if (gamepad1.left_stick_x <= -0.5) {
