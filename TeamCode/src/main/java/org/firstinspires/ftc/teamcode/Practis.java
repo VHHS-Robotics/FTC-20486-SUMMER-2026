d59a9d9ba59d99e8ea7897c341f8178665df7a50
techandrobots <vhhsrobotics@gmail.com>
2026-08-06 10:25:49 -0700

    practis code 26 + C&P 7 + prank 7

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practis.java
@@ -25,17 +25,24 @@ public class Practis extends OpMode {
         LR.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-
     @Override
     public void loop() {
+        double y = -gamepad1.right_stick_y; // Remember, Y stick is reversed!
+        double x = gamepad1.right_stick_x;
+        double rx = gamepad1.left_stick_x;
 
-        if (gamepad1.left_stick_y < -0.5) {
+        LF.setPower(y + x + rx + y + x + rx);
+        LR.setPower(y - x + rx + y - x + rx);
+        RF.setPower(y - x - rx + y - x - rx);
+        RR.setPower(y + x - rx + y + x - rx);
+
+        if (gamepad1.left_stick_y > 0) {
             RR.setDirection(DcMotorSimple.Direction.FORWARD);
             LF.setDirection(DcMotorSimple.Direction.FORWARD);
             LR.setDirection(DcMotorSimple.Direction.FORWARD);
             RF.setDirection(DcMotorSimple.Direction.REVERSE);
         }
-
+/*
         if (gamepad1.left_stick_x <= -0.5) {
             RF.setPower(1);
             RR.setPower(1);
@@ -81,6 +88,6 @@ public class Practis extends OpMode {
                         LR.setPower(0);
                 }
             }
-        }
+        }*/
     }
 }
