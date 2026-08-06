0289830bec06c892ca7ced5b2642b5b3853ec95f
techandrobots <vhhsrobotics@gmail.com>
2026-08-06 09:43:48 -0700

    practis code 19 C&P

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practis.java
@@ -27,7 +27,15 @@ public class Practis extends OpMode {
 
     @Override
     public void loop() {
+        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
 
+        LF.setPower(y + x + rx);
+        LR.setPower(y - x + rx);
+        RF.setPower(y - x - rx);
+        RR.setPower(y + x - rx);
+/*
         if (gamepad1.left_stick_x <= -0.5) {
             RF.setPower(1);
             RR.setPower(1);
@@ -73,6 +81,6 @@ public class Practis extends OpMode {
                         LR.setPower(0);
                 }
             }
-        }
+        }*/
     }
 }
